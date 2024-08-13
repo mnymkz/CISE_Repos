acba6fcb4cbd8255fc14bb58d9e47682b59cb942
mnymkz <michael.sabalburo5@gmail.com>
2024-08-13 15:22:24 +1200

    delete worksheet 3 files

--- worksheet3/worksheet3_frontend/src/app/create-book/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import CreateBookComponent from "@/components/CreateBook";
-export default function CreateBook() {
-    return (
-        <main>
-            <CreateBookComponent />
-        </main>
-    );
-}
--- worksheet3/worksheet3_frontend/src/app/edit-book/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-import UpdateBookInfo from "@/components/UpdateBookInfo";
-export default function ShowBook() {
-    return <UpdateBookInfo />;
-}
